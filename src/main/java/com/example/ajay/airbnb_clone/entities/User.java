713199f560e4333b44dbd74d01044998347f5464
Ajay <ajaybalamurugan11@gmail.com>
2025-11-24 15:42:18 +0530

    Initial Commit

---
+++ src/main/java/com/example/ajay/airbnb_clone/entities/User.java
@@ -0,0 +1,37 @@
+package com.example.ajay.airbnb_clone.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String firstName;
+
+    @Column(nullable = false)
+    private String lastName;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @CreationTimestamp
+    @Column(updatable = false, name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Column(name = "updated_at")
+    private Date updatedAt;
+}
---
+++ src/test/java/com/example/ajay/airbnb_clone/AirbnbCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajay.airbnb_clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirbnbCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
